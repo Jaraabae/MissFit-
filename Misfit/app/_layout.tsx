e8279d33eb4c0f0b2eb22d7c8838d0d67cf9eadb
Jaraabae <128370051+Jaraabae@users.noreply.github.com>
2025-07-20 09:55:47 +0300

    Update _layout.tsx

--- Misfit/app/_layout.tsx
+++ Misfit/app/_layout.tsx
@@ -1,1 +1,14 @@
+import { Stack } from "expo-router";
+
+//This code hides the group (tabs) on top of the screen
+export default function RootLayout() {
+  return (
+      <Stack>
+        <Stack.Screen
+            name="(tabs)"
+            options={{ headerShown: false }}
+        />
+      </Stack>
+  );
+}
 
